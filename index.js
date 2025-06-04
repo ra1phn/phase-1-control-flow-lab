fd10eb9e0cce97f9090f67083866cb4d6dd5603f
Ralph <njugunaralph@gmail.com>
2025-06-04 18:55:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+    if (rideLength <= 400){
+        return 'This one is on me!'
+    } else if (rideLength > 400 && rideLength <= 2000) {
+        return 'That will be twenty bucks.'
+    } else if (rideLength > 2000 && rideLength <= 2500) {
+        return 'I will gladly take your thirty bucks.'
+    } else {
+        return 'No can do.'
+    }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    if (city === 'NYC') {
+        return 'Ok, sounds good.'
+    } else {
+        return 'No go.'
+    }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+        case 'generous':
+            return 'Thank you so much.'
+        case 'not as generous':
+            return 'Thank you.'
+        default:
+            return 'Bye.'
+    }
 }
+
+console.log(scuberGreetingForFeet(199))
+console.log(scuberGreetingForFeet(1500))
+console.log(scuberGreetingForFeet(2500))
+console.log(scuberGreetingForFeet(2501))
+
+console.log(ternaryCheckCity('NYC'))
+console.log(ternaryCheckCity('Pittsburgh')) 
+
+console.log(switchOnCharmFromTip('generous')) 
+console.log(switchOnCharmFromTip('not as generous'))
+console.log(switchOnCharmFromTip('thanks for everything'))
